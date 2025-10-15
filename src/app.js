2873b70c0f8e3e97be5c59d1af984d45412c17bf
Akash Das <akashakki1100@gmail.com>
2025-10-15 21:50:48 +0530

    routing

--- src/app.js
+++ src/app.js
@@ -7,9 +7,22 @@ app.get("/", (req, res) => {
     res.send("Namste AKash")
 })
 
-app.get("/hello", (req, res) => {
-    res.send("Hello hello hola")
+app.get("/user/:userId", (req, res) => {
+    console.log(req.params)
+    res.send({firstName: "Akash", lastName: "Das"})
 })
+
+app.get("/abc", (req, res) => {
+    res.send({firstName: "Akash", lastName: "Das"})
+})
+
+app.post("/user", (req, res) => {
+    res.send("Data successfully saved in the database")
+})
+
+// app.get("/hello", (req, res) => {
+//     res.send("Hello hello hola")
+// })
 
 app.listen(7777, ()=>{
     console.log("Server is Successfully running..... ")
