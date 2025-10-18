21daa9679b00de2ef6d5d269cef27e704498a38a
Akash Das <akashakki1100@gmail.com>
2025-10-19 00:15:14 +0530

    post-dynamic-users

--- src/app.js
+++ src/app.js
@@ -1,29 +1,27 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
-const User = require("./models/user")
+const User = require("./models/user");
 
+app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-  
   //Creating a new Instance of the User model
-  const user = new User({
-    firstName: "Virat",
-    lastName: "Kholi",
-    emailId: "virat@gmail.com",
-    password: "virat@123",
-  })
+  const user = new User(req.body);
 
-  await user.save() // this will give a promise that's why we use await
-  res.send("User added Successfully...")
-
-})
-
-
+  try{
+  await user.save(); // this will give a promise that's why we use await
+  res.send("User added Successfully...");
+  }catch(err){
+  res.status(400).send("Error saving the user :" + err.message)
+  }
+});
 
 connectDB()
   .then(() => {
     console.log("Database connect is successfully");
+    // console.log("About to start server..."); // ✅ ADD THIS
+
     app.listen(7777, () => {
       console.log("Server is Successfully running..... ");
     });
