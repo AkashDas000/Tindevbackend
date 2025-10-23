ad3f65c6141b035d940490323726a3c9a88eb502
Akash Das <akashakki1100@gmail.com>
2025-10-23 23:10:56 +0530

    jwt and cookies implement

--- src/app.js
+++ src/app.js
@@ -2,108 +2,143 @@ const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
-const {validateSignupData} = require("./utils/validation")
-const bycrypt = require("bcrypt")
+const { validateSignupData } = require("./utils/validation");
+const bycrypt = require("bcrypt");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+const { userAuth } = require("./middleware/auth");
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.post("/signup", async (req, res) => {
-  try{
-  //Validate of data
-  validateSignupData(req)
+  try {
+    //Validate of data
+    validateSignupData(req);
 
-  const { firstName, lastName, emailId, password } = req.body
-  //Encrypt password
-  const passwordHash = await bycrypt.hash(password, 10)
-  console.log(passwordHash)
+    const { firstName, lastName, emailId, password } = req.body;
+    //Encrypt password
+    const passwordHash = await bycrypt.hash(password, 10);
+    console.log(passwordHash);
 
-  //Creating a new Instance of the User model
-  const user = new User({
-    firstName, lastName, emailId, password: passwordHash,
-  });
+    //Creating a new Instance of the User model
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHash,
+    });
 
-  await user.save(); // this will give a promise that's why we use await
-  res.send("User added Successfully...");
-  }catch(err){
-  res.status(400).send("ERROR : " + err.message)
+    await user.save(); // this will give a promise that's why we use await
+    res.send("User added Successfully...");
+  } catch (err) {
+    res.status(400).send("ERROR : " + err.message);
   }
 });
 
 app.post("/login", async (req, res) => {
-  try{
-    const { emailId, password} = req.body;
+  try {
+    const { emailId, password } = req.body;
 
-    const user = await User.findOne({emailId: emailId})
-    if(!user){
-      throw new Error("Invalid Email")
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
+      throw new Error("Invalid Email");
     }
 
-    const passwordValid = await bycrypt.compare(password, user.password)
-    if(passwordValid){
-      res.send("Login Succesfully !")
-    }else{
-      throw new Error("Invalid password")
-    }
+    const passwordValid = await bycrypt.compare(password, user.password);
+    if (passwordValid) {
+      //Create JWT Token
+      const token = await jwt.sign({ _id: user._id }, "Dev@Tinder456");
 
-  }catch(err){
-  res.status(400).send("ERROR : " + err.message)
+      //Add the token to cookie and send the response back to the user
+      res.cookie("token", token);
+      res.send("Login Succesfully !");
+    } else {
+      throw new Error("Invalid password");
+    }
+  } catch (err) {
+    res.status(400).send("ERROR : " + err.message);
   }
-})
+});
+
+app.get("/profile", userAuth, async (req, res) => {
+  try {
+    const user = req.user;
+
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("ERROR : " + err.message);
+  }
+});
+
+app.post("/sendConnectionRequest", userAuth, async (req, res) => {
+  try {
+    const user = req.user;
+    //Sending a connection request
+    console.log("Sending a connection is request");
+
+    res.send(user.firstName + " sent the connect request!");
+  } catch (err) {
+    res.status(400).send("ERROR : " + err.message);
+  }
+});
 
 app.get("/user", async (req, res) => {
   const userEmail = req.body.emailId;
 
-  try{
-    const user = await User.find({emailId: userEmail})
-    if(user.length == 0){
-      res.send("User not found")
-    }else{
-      res.send(user)
+  try {
+    const user = await User.find({ emailId: userEmail });
+    if (user.length == 0) {
+      res.send("User not found");
+    } else {
+      res.send(user);
     }
-  }catch(err){
-    res.status(404).send("Something wend wrong")
+  } catch (err) {
+    res.status(404).send("Something wend wrong");
   }
-})
+});
 
 app.get("/feed", async (req, res) => {
-  try{
-    const users = await User.find({})
-    res.send(users)
-  }catch(err){
-    res.status(404).send("Something wend wrong")
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(404).send("Something wend wrong");
   }
-})
+});
 
 app.delete("/user", async (req, res) => {
-  const userId = req.body.userId
+  const userId = req.body.userId;
 
-  try{
-    const user = await User.findByIdAndDelete(userId)
-    res.send("User deleted successfully")
-  }catch(err){
-    res.status(404).send("Something wend wrong")
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    res.send("User deleted successfully");
+  } catch (err) {
+    res.status(404).send("Something wend wrong");
   }
-})
+});
 
 app.patch("/user/:id", async (req, res) => {
-  const id = req.params?.id
-  const data = req.body
+  const id = req.params?.id;
+  const data = req.body;
   // console.log(data)
-  try{
-    const Allowed_Updates = ["gender","age","skills"]
-    const isUpdateAllowed = Object.keys(data).every((k) => Allowed_Updates.includes(k));
-    if(!isUpdateAllowed){
-      throw new Error("Update not allowed")
+  try {
+    const Allowed_Updates = ["gender", "age", "skills"];
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      Allowed_Updates.includes(k)
+    );
+    if (!isUpdateAllowed) {
+      throw new Error("Update not allowed");
     }
-    if(data?.skills.length > 10){
-      throw new Error("Skills cannot be more than 10")
+    if (data?.skills.length > 10) {
+      throw new Error("Skills cannot be more than 10");
     }
-    await User.findByIdAndUpdate({_id: id}, data)
-    res.send("User update successfully!")
-  }catch(err){
-    res.status(404).send("Something wend wrong " + err.message)
-  } 
-})
+    await User.findByIdAndUpdate({ _id: id }, data);
+    res.send("User update successfully!");
+  } catch (err) {
+    res.status(404).send("Something wend wrong " + err.message);
+  }
+});
 
 // app.get("/findId", async (req, res) => {
 //   try{
@@ -117,7 +152,7 @@ app.patch("/user/:id", async (req, res) => {
 connectDB()
   .then(() => {
     console.log("Database connect is successfully");
-    // console.log("About to start server..."); // ✅ ADD THIS
+    // console.log("About to start server...");
 
     app.listen(7777, () => {
       console.log("Server is Successfully running..... ");
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,14 +1,31 @@
-const adminAuth = (req, res, next) => {
-    console.log("Admin authorized is getting checked !")
-    const token = "abc"
-    const isAuthorized = token === "abc"
-    if(!isAuthorized){
-        res.send("Unauthrized token")
-    }else{
-        next()
+const jwt = require("jsonwebtoken")
+const User = require("../models/user")
+
+const userAuth = async (req, res, next) => {
+   try{
+
+    const {token} = req.cookies
+    if(!token){
+        throw new Error("Invalid Token ")
     }
+
+    const decodeObj = await jwt.verify(token, "Dev@Tinder456")
+
+    const {_id} = decodeObj
+
+    const user = await User.findById(_id);
+    if(!user){
+        throw new Error("User not found")
+    }
+
+    req.user = user
+    next()
+
+   }catch(err){
+    res.status(400).send("Error: " + err.message)
+   }
 }
 
 module.exports = {
-    adminAuth
+    userAuth,
 }
